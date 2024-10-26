ad4806370cbca50c521965f5570c45f31b06bb21
Kim Long Ngo <ngokimlong2007@gmail.com>
2024-10-26 14:20:47 -0400

    Initial Commit

---
+++ src/main/java/kimlngo/springframework/reactive/streaming_stock_quote_service/StreamingStockQuoteServiceApplication.java
@@ -0,0 +1,13 @@
+package kimlngo.springframework.reactive.streaming_stock_quote_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingStockQuoteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingStockQuoteServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kimlngo/springframework/reactive/streaming_stock_quote_service/StreamingStockQuoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kimlngo.springframework.reactive.streaming_stock_quote_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingStockQuoteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
